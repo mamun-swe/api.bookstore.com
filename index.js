52d6168c1310e6d4c24df679af68ace923c5a2d3
Mamun-swe <mhmamun166009@gmail.com>
2022-06-05 01:33:42 +0600

    App port changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("dotenv").config()
 
 const main = async () => {
     const DB_URL = process.env.DB_URL
-    const PORT = process.env.APP_PORT || 8080
+    const PORT = process.env.APP_PORT || 5000
 
     const app = express()
 
